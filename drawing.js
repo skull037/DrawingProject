cdba18060f10472cb2520aeffc286b7c70376d12
max sami <skull037037@gmail.com>
2016-10-04 15:55:01 -0700

    Oct 4 3:54

--- drawing.js
+++ drawing.js
@@ -22,19 +22,25 @@ canvas.fillStyle = color;
 canvas.fill();
 
 }
+//14
 //background
 drawCircle("#fff", 650, 25, 50, 0,2*Math.PI);
 drawCircle("#fff", 675, 25, 5, 0,2*Math.PI);
 //city
 drawRectangle("#233", 600,100,100,600);
 
-
 drawRectangle("gray", 433, 300, 100, 700);
 drawTriangle("gray", 533, 300,433,300,533,200);
 
 drawTriangle("#555", 200, 300,200,400,300,400);
 drawTriangle("#555", 200, 300,200,200,300,200);
+drawTriangle("#555", 150, 200,300,100,300,200);
 drawRectangle("#555", 150, 200, 50, 700);
 drawRectangle("#555", 200, 400, 100, 400);
+
+drawTriangle("gray", 800, 600,900,500,1000,600);
+drawTriangle("gray", 800, 600,1000,500,1000,600);
+drawTriangle("gray", 800, 600,800,500,1000,600);
+
 //foreground
 drawRectangle("green", 0,575,25000,100);
