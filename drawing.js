b8264a21939fa7973c310b33e83e9971b7ac7df6
max sami <skull037037@gmail.com>
2016-10-04 16:13:15 -0700

    Oct 4 4:12
    realized I forgot had a commit descrition last time.Added hills and
    another building

--- drawing.js
+++ drawing.js
@@ -5,6 +5,7 @@ var canvas = canvasDiv.getContext('2d');
 function drawRectangle(color, xCoord, yCoord,xWidth,yWidth) {
   canvas.fillStyle = color;
   canvas.fillRect(xCoord, yCoord, xWidth, yWidth);
+    canvas.stroke();
 }
 function drawTriangle(color,firstxCoord,firstyCoord,secondxCoord,secondyCoord,thirdxCoord,thirdyCoord){
   canvas.beginPath();
@@ -13,6 +14,7 @@ canvas.lineTo(secondxCoord,secondyCoord);
 canvas.lineTo(thirdxCoord,thirdyCoord);
   canvas.fillStyle = color;
 canvas.fill();
+canvas.stroke();
 }
 function drawCircle(color,x, y, radius, startAngle, endAngle){
   canvas.beginPath();
@@ -22,10 +24,18 @@ canvas.fillStyle = color;
 canvas.fill();
 
 }
-//14
+//19
 //background
 drawCircle("#fff", 650, 25, 50, 0,2*Math.PI);
-drawCircle("#fff", 675, 25, 5, 0,2*Math.PI);
+drawCircle("#eee", 675, 25, 5, 0,2*Math.PI);
+
+//background hills
+drawCircle("#090", 1300, 600, 300, 0,2*Math.PI);
+drawCircle("#090", 1000, 600, 250, 0,2*Math.PI);
+drawCircle("#090", 1300, 600, 150, 0,2*Math.PI);
+
+drawCircle("#090", 100, 600, 250, 0,2*Math.PI);
+drawCircle("#090", 100, 600, 150, 0,2*Math.PI);
 //city
 drawRectangle("#233", 600,100,100,600);
 
@@ -38,7 +48,7 @@ drawTriangle("#555", 150, 200,300,100,300,200);
 drawRectangle("#555", 150, 200, 50, 700);
 drawRectangle("#555", 200, 400, 100, 400);
 
-drawTriangle("gray", 800, 600,900,500,1000,600);
+drawTriangle("#aaa", 800, 600,900,500,1000,600);
 drawTriangle("gray", 800, 600,1000,500,1000,600);
 drawTriangle("gray", 800, 600,800,500,1000,600);
 
