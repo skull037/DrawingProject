1e819e190b31dfecf071ebd880f945ebc24518c2
max sami <skull037037@gmail.com>
2016-10-05 08:58:39 -0700

    Oct 5 8:58
    added mountains to each side of the canvas increasing the shape count to
    24.

--- drawing.js
+++ drawing.js
@@ -1,12 +1,12 @@
 var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
 
-
 function drawRectangle(color, xCoord, yCoord,xWidth,yWidth) {
   canvas.fillStyle = color;
   canvas.fillRect(xCoord, yCoord, xWidth, yWidth);
     canvas.stroke();
 }
+
 function drawTriangle(color,firstxCoord,firstyCoord,secondxCoord,secondyCoord,thirdxCoord,thirdyCoord){
   canvas.beginPath();
 canvas.moveTo(firstxCoord,firstyCoord);
@@ -16,6 +16,7 @@ canvas.lineTo(thirdxCoord,thirdyCoord);
 canvas.fill();
 canvas.stroke();
 }
+
 function drawCircle(color,x, y, radius, startAngle, endAngle){
   canvas.beginPath();
 canvas.arc(x, y, radius, startAngle, endAngle);
@@ -24,12 +25,16 @@ canvas.fillStyle = color;
 canvas.fill();
 
 }
-//21
+
+//24
 //background
 drawCircle("#fff", 650, 25, 50, 0,2*Math.PI);
 drawCircle("#eee", 675, 25, 5, 0,2*Math.PI);
 
 //background hills
+drawTriangle("#555", 1350, 100,1200,500,13500,600);
+drawTriangle("#555", 0, 100,100,500,0,600);
+
 drawCircle("#090", 1300, 600, 300, 0,2*Math.PI);
 drawCircle("#090", 1000, 600, 250, 0,2*Math.PI);
 drawCircle("#090", 1300, 600, 150, 0,2*Math.PI);
@@ -37,6 +42,7 @@ drawCircle("#090", 1300, 600, 150, 0,2*Math.PI);
 drawCircle("#090", 100, 600, 250, 0,2*Math.PI);
 drawCircle("#090", 0, 600, 150, 0,2*Math.PI);
 drawCircle("#090", 100, 600, 50, 0,2*Math.PI);
+
 //city
 drawRectangle("#233", 600,100,100,600);
 
@@ -53,6 +59,7 @@ drawTriangle("#aaa", 800, 600,900,500,1000,600);
 drawTriangle("gray", 800, 600,1000,500,1000,600);
 drawTriangle("gray", 800, 600,800,500,1000,600);
 
+drawTriangle("gray", 1100, 299,1100,200,1150,299);
 drawRectangle("#gray", 1100,300,100,600);
 
 //foreground
